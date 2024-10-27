707dccc11301db2e75de36d00957c3dfec8c0980
JourneyCherry <sunny_jey@naver.com>
2024-10-27 15:50:17 +0900

    위젯 유닛테스트 예시코드 추가

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,4 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-void main() {}
+import 'package:filetagger/Widgets/list_widget.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('test ListView Widget', (WidgetTester tester) async {
+    await tester.pumpWidget(const ListWidget());
+    //TODO : ListWidget에 아이템들을 넣어 제대로 데이터를 표시하는지 확인
+  });
+}

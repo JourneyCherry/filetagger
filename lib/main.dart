a33c5cdf35b3f67136c6aa975b5ba1d4afe5d8ed
JourneyCherry <sunny_jey@naver.com>
2024-12-07 17:51:58 +0900

    디렉토리 선택 창 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:filetagger/DataStructures/directory_reader.dart';
 import 'package:filetagger/Widgets/list_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:multi_split_view/multi_split_view.dart';
@@ -18,16 +20,32 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MultiSplitViewTheme(
-        data: MultiSplitViewThemeData(
-            dividerPainter: DividerPainters.grooved1(
-                color: Colors.indigo[100]!,
-                highlightedColor: Colors.indigo[400]!)),
-        child: MultiSplitView(
-          initialAreas: [
-            Area(builder: (context, area) => Draft.blue()),
-            Area(builder: (context, area) => ListWidget()),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("FileTagger"),
+          centerTitle: true,
+          leading: Tooltip(
+            message: "Open Directory",
+            child: IconButton(
+              onPressed: () async {
+                final path = await FilePicker.platform.getDirectoryPath();
+                DirectoryReader().readDirectory(path);
+              },
+              icon: Icon(Icons.file_copy),
+            ),
+          ),
+        ),
+        body: MultiSplitViewTheme(
+          data: MultiSplitViewThemeData(
+              dividerPainter: DividerPainters.grooved1(
+                  color: Colors.indigo[100]!,
+                  highlightedColor: Colors.indigo[400]!)),
+          child: MultiSplitView(
+            initialAreas: [
+              Area(builder: (context, area) => Draft.blue()),
+              Area(builder: (context, area) => ListWidget()),
+            ],
+          ),
         ),
       ),
     );
